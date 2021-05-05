7faee4eb6307f92034caade58732282bc98ef8c9
shunkominato <shun19871001@gmail.com>
2021-05-05 21:56:32 +0900

    migration  add

---
+++ src/migration/1620219315202-user.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class user1620219315202 implements MigrationInterface {
+    name = 'user1620219315202'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query("CREATE TABLE `user` (`id` int NOT NULL AUTO_INCREMENT, `firstName` varchar(255) NOT NULL, `lastName` varchar(255) NOT NULL, `age` int NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB");
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query("DROP TABLE `user`");
+    }
+
+}
